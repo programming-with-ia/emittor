eae36e4d5841a192871c75fa0b3c9ff160b39be7
immi <iafullprogrammer@gmail.com>
2024-10-30 14:22:27 +0500

    v0.2.0-beta.1

--- src/useEmittor.ts
+++ src/useEmittor.ts
@@ -1,27 +1,47 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
-import { Emittor } from './createEmittor';
+import { useEffect, useState } from "react";
+import { Emittor } from "./createEmittor";
 
-export const useEmittor = <T>(emittor: Emittor<T>): [T, ((state: T | ((state: T) => T)) => void)] => {
-    const [state, setState] = useState(emittor.state);
-
-    const setValue = (state: T | ((state: T) => T)) => {
-        let newState = state as T
-
-        if (state instanceof Function) {
-            newState = state(emittor.state)
-        }
-        emittor.emit(newState)
-    }
-
-    useEffect(() => {
-        emittor.connect(setState);
-
-        // Disconnect on unmount
-        return () => {
-            emittor.disconnect(setState);
-        };
-    }, [emittor]); // Run effect again if emittor changes
-
-    return [state, setValue]
+function isFunc<T>(value: unknown): value is (state: T) => T {
+  return typeof value === "function";
 }
+
+export const useEmittor = <T>(
+  emittor: Emittor<T>,
+  defaultValue?: T | (() => T)
+): [T, (state: T | ((state: T) => T)) => void] => {
+  const [state, setState] = useState<T>(() => {
+    // Initialize state with the emittor state or the default value
+    if (defaultValue !== undefined && emittor.state === undefined) {
+      const initial = isFunc<T>(defaultValue) ? defaultValue() : defaultValue;
+      emittor.emit(initial);
+      return initial;
+    }
+    return emittor.state;
+  });
+
+  const setValue = (newState: T | ((state: T) => T)) => {
+    const updatedState = isFunc<T>(newState) ? newState(state) : newState;
+    emittor.emit(updatedState);
+  };
+
+  // useEffect(() => {
+  //     // Emit default value if the emittor state is undefined (only set in emit)
+  //     if (defaultValue !== undefined && emittor.state === undefined) {
+  //         console.log("defaultValue", defaultValue)
+  //         const initial = isFunc<T>(defaultValue) ? defaultValue() : defaultValue;
+  //         emittor.emit(initial);
+  //     }
+  // }, [defaultValue, emittor]);
+
+  useEffect(() => {
+    const updateState = (newState: T) => setState(() => newState);
+    emittor.connect(updateState);
+
+    return () => {
+      emittor.disconnect(updateState);
+    };
+  }, [emittor]);
+
+  return [state, setValue];
+};
